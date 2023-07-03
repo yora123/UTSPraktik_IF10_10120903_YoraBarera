24d554884d29f2601e143bf4ed267dcc5059f686
yora123 <yorabarera@gmail.com>
2023-07-03 22:37:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/utspraktik_if10_10120903_yorabarera/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.utspraktik_if10_10120903_yorabarera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
